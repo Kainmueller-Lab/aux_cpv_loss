91c28326b9d7fd4f6f593275e1a7bdb764c82845
Peter <peter.hirsch@mdc-berlin.de>
2020-03-24 16:56:07 +0100

    update default config

--- auxcpvloss/util/__init__.py
+++ auxcpvloss/util/__init__.py
@@ -1,5 +1,4 @@
 from .unet import unet, conv_pass, crop
-from .unet_for_tests import unet_ext
 from .losses import get_loss, get_loss_weighted
 from .selectGPU import selectGPU
 from .gp_noop import NoOp
