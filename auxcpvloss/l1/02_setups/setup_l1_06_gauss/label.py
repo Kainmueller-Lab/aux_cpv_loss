57845c53863d40e3594f6e5af32d5e8661b78039
Peter Hirsch <abred@users.noreply.github.com>
2020-03-24 15:29:54 +0100

    update gauss label.py

--- auxcpvloss/l1/02_setups/setup_l1_06_gauss/label.py
+++ auxcpvloss/l1/02_setups/setup_l1_06_gauss/label.py
@@ -5,7 +5,8 @@ import h5py
 import mahotas
 import numpy as np
 import zarr
-
+import skimage.feature
+import scipy.ndimage
 logger = logging.getLogger(__name__)
 
 
@@ -27,27 +28,65 @@ def label(**kwargs):
     logger.debug("prediction shape %s", gauss.shape)
     logger.debug("raw shape %s", raw.shape)
 
-    # load gt image
-    if kwargs['gt_format'] == "hdf":
-        with h5py.File(kwargs['gt'], 'r') as gt:
-            gt_labels = np.array(gt[kwargs['gt_key']])
-    elif kwargs['gt_format'] == "zarr":
-        gt = zarr.open(kwargs['gt'], 'r')
-        gt_labels = np.array(gt[kwargs['gt_key']])
-    else:
-        raise NotImplementedError("invalid gt format")
-    gt_labels = np.squeeze(gt_labels, axis=0)
-    logger.debug("%s: gt min %f, max %f",
-                 kwargs['sample'], gt_labels.min(), gt_labels.max())
+    # # load gt image
+    # if kwargs['gt_format'] == "hdf":
+    #     with h5py.File(kwargs['gt'], 'r') as gt:
+    #         gt_labels = np.array(gt[kwargs['gt_key']])
+    # elif kwargs['gt_format'] == "zarr":
+    #     gt = zarr.open(kwargs['gt'], 'r')
+    #     gt_labels = np.array(gt[kwargs['gt_key']])
+    # else:
+    #     raise NotImplementedError("invalid gt format")
+    # gt_labels = np.squeeze(gt_labels, axis=0)
+    # logger.debug("%s: gt min %f, max %f",
+    #              kwargs['sample'], gt_labels.min(), gt_labels.max())
 
 
-    markers = 1*(gauss > kwargs['gauss_thresh'])
-    logger.debug("pixel over threshold: %s (%s)", np.argwhere(markers).shape,
-                 markers.dtype)
-    if np.argwhere(markers).shape[0] == 0:
-        logger.error("no blobs found for %s (0 pixel over threshold %s)",
-                     kwargs['sample'], kwargs['gauss_thresh'])
-        return
+    # markers = 1*(gauss > kwargs['gauss_thresh'])
+    # logger.debug("pixel over threshold: %s (%s)", np.argwhere(markers).shape,
+    #              markers.dtype)
+    # if np.argwhere(markers).shape[0] == 0:
+    #     logger.error("no blobs found for %s (0 pixel over threshold %s)",
+    #                  kwargs['sample'], kwargs['gauss_thresh'])
+    #     return
+
+    # if kwargs['debug']:
+    #     output_file.create_dataset('volumes/gauss', data=gauss,
+    #                                dtype=np.float32, compression='gzip')
+
+
+    # if kwargs['debug']:
+    #     output_file.create_dataset('volumes/markers1',
+    #                                data=markers.astype(np.uint16),
+    #                                compression='gzip')
+
+    # reg_max = 1*mahotas.regmax(gauss)
+    # if kwargs['debug']:
+    #     output_file.create_dataset('volumes/markers2',
+    #                                data=reg_max.astype(np.uint16),
+    #                                compression='gzip')
+    # logger.debug("regional maxima: %s (%s)", np.argwhere(reg_max > 0).shape,
+    #              markers.dtype)
+
+    # reg_max = markers * reg_max
+    # logger.debug("regional maxima over threshold: %s",
+    #              np.argwhere(reg_max > 0).shape)
+    # reg_max = reg_max.astype(np.uint16)
+    # # TODO: check
+    # gaussTmp = gauss.copy()
+    # gaussTmp[gauss < kwargs['gauss_thresh']] = 0
+    # gaussTmp = 1*mahotas.regmax(gaussTmp)
+    # reg_max = gaussTmp
+
+    gaussTmp = gauss.copy()
+    min_distance = kwargs['nms_size']
+    size = 2 * min_distance + 1
+    image_max = scipy.ndimage.maximum_filter(gaussTmp, size=size, mode='constant')
+    mask = gaussTmp == image_max
+    mask &= gaussTmp > kwargs['gauss_thresh']
+    print(np.count_nonzero(mask))
+    reg_max = mask
+
 
     # create output file
     if kwargs['output_format'] == "hdf":
@@ -57,35 +96,9 @@ def label(**kwargs):
     else:
         raise NotImplementedError("invalid output format")
 
-    if kwargs['debug']:
-        output_file.create_dataset('volumes/gauss', data=gauss,
-                                   dtype=np.float32, compression='gzip')
-
-
-    if kwargs['debug']:
-        output_file.create_dataset('volumes/markers1',
-                                   data=markers.astype(np.uint16),
-                                   compression='gzip')
-
-    reg_max = 1*mahotas.regmax(gauss)
-    if kwargs['debug']:
-        output_file.create_dataset('volumes/markers2',
-                                   data=reg_max.astype(np.uint16),
-                                   compression='gzip')
-    logger.debug("regional maxima: %s (%s)", np.argwhere(reg_max > 0).shape,
-                 markers.dtype)
-
-    reg_max = markers * reg_max
-    logger.debug("regional maxima over threshold: %s",
-                 np.argwhere(reg_max > 0).shape)
-    reg_max = reg_max.astype(np.uint16)
-    # TODO: check
-    gaussTmp = gauss.copy()
-    gaussTmp[gauss < kwargs['gauss_thresh']] = 0
-    gaussTmp = 1*mahotas.regmax(gaussTmp)
-    reg_max = gaussTmp
-
     output_file.create_dataset('volumes/markers', data=reg_max,
+                               dtype=np.float32, compression='gzip')
+    output_file.create_dataset('volumes/pred_cp', data=gauss,
                                dtype=np.float32, compression='gzip')
     output_file.create_dataset('volumes/raw_cropped', data=raw,
                                compression='gzip')
